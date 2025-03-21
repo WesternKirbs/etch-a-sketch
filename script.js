0c16b94c7cf6b53c9847d1bda44a605e65fb72db
WesternKirbs <benlin96186@gmail.com>
2025-03-21 16:37:22 +0800

    updated button for choosing grid size

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
 const container = document.querySelector(".container");
 let n = 16;
-let grid = []
-for(let i = 0; i<n; i++){
-    var row = document.createElement("div");
-    row.setAttribute("class", "row");
-    row.setAttribute("style", "width:960px; display:flex;");
+var pushed = 0;
+const btn = document.createElement("button");
+btn.textContent = "choose grid size";
 
-    for(let j = 0; j<n; j++){
-
-        var block = document.createElement("div");
-        block.setAttribute("class", "block_default");
-        /*
-        if(!j){
-            grid[i] = [block];
-            continue;
-        }
-        grid[i].push(block); */
-        block.addEventListener("mouseover", (e) =>{
-            e.target.classList.toggle("block_on");
-        });
-        block.addEventListener("mouseout", (e) =>{
-            e.target.classList.toggle("block_on");
-        });
-        row.appendChild(block);
+btn.addEventListener("click", (e) =>{
+    if(pushed != 0){
+        container.removeChild(document.getElementById('grid'));
     }
-    container.appendChild(row);
-}
+    const grid = document.createElement("div");
+    grid.setAttribute("id","grid");
+    n = prompt("choose grid size: ");
+    for(let i = 0; i<n; i++){
+        var row = document.createElement("div");
+        row.setAttribute("class", "row");
+        row.setAttribute("style", "width:960px; display:flex;");
+    
+        for(let j = 0; j<n; j++){
+            var block = document.createElement("div");
+            let w = 960 / n;
+            block.setAttribute("class", "block_default");
+            block.setAttribute("style", ` width:${w}px; height:${w}px;`);
+            block.addEventListener("mouseover", (e) =>{
+                e.target.classList.add("block_on");
+            });
+
+            row.appendChild(block);
+        }
+        grid.appendChild(row);
+    }
+    container.appendChild(grid);
+    pushed += 1;
+})
+container.appendChild(btn);
+
+
+
